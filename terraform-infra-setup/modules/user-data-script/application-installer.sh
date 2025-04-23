52282eb3c890680b6c9f20b41356e9c4c9753e10
KushalNishad <nkushal77@gmail.com>
2025-04-22 21:20:20 -0400

    Updated application-installer.sh

--- terraform-infra-setup/modules/user-data-script/application-installer.sh
+++ terraform-infra-setup/modules/user-data-script/application-installer.sh
@@ -6,12 +6,12 @@ yes | sudo apt update
 yes | sudo apt install python3.12 python3.12-venv python3-pip python3-dev default-libmysqlclient-dev build-essential git -y
 
 # Set default python3 and pip to python3.12
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.12 1
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.12 1
 
 # Clone your repo
 git clone https://github.com/KushalNishad/Dockerized-Flask-app.git
-cd FinTech-Flask-app
+cd Dockerized-Flask-app
 
 # Create and activate virtual environment
 python3.12 -m venv venv
